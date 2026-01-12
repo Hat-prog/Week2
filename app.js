8f2fc5eb9ed510913c21bb25418420a76a8d091a
Lukas <thokenm3@gmail.com>
2026-01-12 13:55:27 +0000

    Added my favourite foods

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log ("Hello, World!");
+const favFoods = [
+  "Pizza",
+  "Burgers",
+  "Lasagna",
+];
+
+console.log(favFoods); // 
+
+const randomIndex = Math.floor(Math.random() * favFoods.length);
+console.log(favFoods[randomIndex]); 
